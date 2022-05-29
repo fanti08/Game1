4f7858765c5d9dd3905197f939b356a35489da6f
fanti08 <stete.fanti@gmail.com>
2022-05-29 12:36:00 +0200

    FIRST VERSION EVER
    Player movement

--- Assets/SCRIPTS/PlayerMovement.cs
+++ Assets/SCRIPTS/PlayerMovement.cs
@@ -12,16 +12,17 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private bool isGrounded;
     [SerializeField] private bool hasSmthAboveHead;
     [SerializeField] private bool wasGoingRight;
-    [SerializeField] [Range(1, 50)] private int maxSpeed = 12;
-    [SerializeField] [Range(1, 50)] private int jumpForce = 12;
+    [Range(1, 100)] public int maxSpeed;
+    [Range(1, 100)] public float jumpForce;
+    [SerializeField] private int _maxSpeed;
+    [SerializeField] private float _jumpForce;
     [SerializeField] private bool A;
     [SerializeField] private bool D;
     [SerializeField] private bool space;
     [SerializeField] private bool shift;
     [SerializeField] private LayerMask ground;
-    [SerializeField] private float lastTimeGrounded;
-    [SerializeField] private float lastTimeJumped;
-    [SerializeField] private bool hasJumped;
+    [SerializeField] private float coyoteTime = .2f;
+    [SerializeField] private float coyoteCounter;
     [Header("ANIMATIONS")]
     [SerializeField] private Animator anim;
 
@@ -50,10 +51,10 @@ public class PlayerMovement : MonoBehaviour
     {
         currentHorizontalSpeed = rb.velocity.x;
         currentVerticalSpeed = rb.velocity.y;
-        isGrounded = Physics2D.BoxCast(bc.bounds.center, bc.bounds.size, 0, Vector2.down, .01f, ground);
+        isGrounded = Physics2D.BoxCast(bc.bounds.center, bc.bounds.size, 0, Vector2.down, .02f, ground);
         if (currentHorizontalSpeed > 0) wasGoingRight = true;
         if (currentHorizontalSpeed < 0) wasGoingRight = false;
-        hasSmthAboveHead = Physics2D.BoxCast(bc.bounds.center, bc.bounds.size, 0, Vector2.up, .75f, ground); 
+        hasSmthAboveHead = Physics2D.BoxCast(bc.bounds.center, bc.bounds.size, 0, Vector2.up, .75f, ground);
     }
 
     private void DefineKeys()
@@ -71,44 +72,53 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Accelerate()
     {
-        Vector2 DspeedToAccel = new Vector2(rb.velocity.x + Time.deltaTime * 20, currentVerticalSpeed);
-        Vector2 DspeedToAccelinAir = new Vector2(rb.velocity.x + Time.deltaTime * 20 / 2, currentVerticalSpeed); 
-        Vector2 AspeedToAccel = new Vector2(rb.velocity.x + Time.deltaTime * -20, currentVerticalSpeed);
-        Vector2 AspeedToAccelinAir = new Vector2(rb.velocity.x + Time.deltaTime * -20 / 2, currentVerticalSpeed);
-        if (Mathf.Abs(currentHorizontalSpeed) < maxSpeed)
+        Vector2 DspeedToAccel = new Vector2(rb.velocity.x + Time.deltaTime * 35, currentVerticalSpeed);
+        Vector2 DspeedToAccelinAir = new Vector2(rb.velocity.x + Time.deltaTime * 35 / 1.5f, currentVerticalSpeed); 
+        Vector2 AspeedToAccel = new Vector2(rb.velocity.x + Time.deltaTime * -35, currentVerticalSpeed);
+        Vector2 AspeedToAccelinAir = new Vector2(rb.velocity.x + Time.deltaTime * -35 / 1.5f, currentVerticalSpeed);
+        if (Mathf.Abs(currentHorizontalSpeed) < _maxSpeed)
         {
             if (A) rb.velocity = isGrounded ? AspeedToAccelinAir : AspeedToAccel;
             if (D) rb.velocity = isGrounded ? DspeedToAccelinAir : DspeedToAccel;
         }
+        if (currentHorizontalSpeed > _maxSpeed - .05f) rb.velocity = new Vector2(_maxSpeed, currentVerticalSpeed);
+        if (currentHorizontalSpeed < -_maxSpeed + .05f) rb.velocity = new Vector2(-_maxSpeed, currentVerticalSpeed);
     }
     private void Decelerate()
     {
-        Vector2 DspeedToDecel = new Vector2(rb.velocity.x - Time.deltaTime * 20, currentVerticalSpeed);
-        Vector2 DspeedToDecelinAir = new Vector2(rb.velocity.x - Time.deltaTime * 20 / 2, currentVerticalSpeed);
-        Vector2 AspeedToDecel = new Vector2(rb.velocity.x + Time.deltaTime * 20, currentVerticalSpeed);
-        Vector2 AspeedToDecelinAir = new Vector2(rb.velocity.x + Time.deltaTime * 20 / 2, currentVerticalSpeed);
+        Vector2 DspeedToDecel = new Vector2(rb.velocity.x - Time.deltaTime * 35, currentVerticalSpeed);
+        Vector2 DspeedToDecelinAir = new Vector2(rb.velocity.x - Time.deltaTime * 35 / 1.5f, currentVerticalSpeed);
+        Vector2 AspeedToDecel = new Vector2(rb.velocity.x + Time.deltaTime * 35, currentVerticalSpeed);
+        Vector2 AspeedToDecelinAir = new Vector2(rb.velocity.x + Time.deltaTime * 35 / 1.5f, currentVerticalSpeed);
         if (Mathf.Abs(currentHorizontalSpeed) > 0)
         {
             if (!wasGoingRight) rb.velocity = isGrounded ? AspeedToDecelinAir : AspeedToDecel;
             else rb.velocity = isGrounded ? DspeedToDecelinAir : DspeedToDecel;
         }
+        if (Mathf.Abs(currentHorizontalSpeed) < .05f) rb.velocity = new Vector2(0, currentVerticalSpeed);
     }
 
     private void Jump()
     {
-        if (isGrounded && space)
+        if (space && coyoteCounter > 0)
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            lastTimeGrounded = 0;
-            lastTimeJumped = 0;
-            hasJumped = true;
+            rb.velocity = new Vector2(currentHorizontalSpeed * 1.3f, _jumpForce);
+            coyoteCounter = 0;
         }
         CoyoteTime();
     }
     private void CoyoteTime()
     {
-        lastTimeJumped += Time.deltaTime;
-        lastTimeGrounded += Time.deltaTime;
+        if (isGrounded)
+        {
+            coyoteCounter = coyoteTime;
+            rb.gravityScale = 1;
+        }
+        else
+        {
+            coyoteCounter -= Time.deltaTime;
+            rb.gravityScale = 3;
+        }
     }
 
     private void Crouch()
@@ -118,7 +128,8 @@ public class PlayerMovement : MonoBehaviour
             GameObject.Find("Sprites").transform.localScale = new Vector3(1, .5f, 1);
             GameObject.Find("Sprites").transform.localPosition = new Vector3(0, -5.125f - 7 / 6, 0);
             bc.size = new Vector2(6, 7.5f);
-            maxSpeed = 3;
+            _maxSpeed = maxSpeed / 4;
+            _jumpForce = jumpForce / 1.5f;
             bc.offset = new Vector2(0, -3.725f);
         }
         else if (!hasSmthAboveHead)
@@ -126,7 +137,8 @@ public class PlayerMovement : MonoBehaviour
             GameObject.Find("Sprites").transform.localScale = new Vector3(1, 1, 1);
             GameObject.Find("Sprites").transform.localPosition = new Vector3(0, -5.125f, 0);
             bc.size = new Vector2(6, 15);
-            maxSpeed = 12;
+            _maxSpeed = maxSpeed;
+            _jumpForce = jumpForce;
             bc.offset = new Vector2(0, 0);
         }
     }
